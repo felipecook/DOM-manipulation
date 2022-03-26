f8d5a98a506d826b838b411f130334699fc5e6a3
felipe cook <felipepcook@gmail.com>
2022-03-26 18:05:46 -0400

    adding event listeners and learning about targets

--- app.js
+++ app.js
@@ -12,8 +12,52 @@ container.appendChild(content);
 const paragraphContent = document.createElement('p');
 paragraphContent.classList.add('paragraph');
 paragraphContent.textContent = 'Hey I\'m red!';
+paragraphContent.style.color = 'red';
 
 
 content.appendChild(paragraphContent);
+
+//Adds h3 to the content parent after the paragraph child
+const header3Content = document.createElement('h3');
+header3Content.textContent = 'I\'m a blue h3';
+header3Content.style.color = 'blue';
+
+content.appendChild(header3Content);
+
+
+//add div with black border and pink background color to content
+const innerDiv =  document.createElement('div');
+innerDiv.style.borderStyle = 'dashed';
+innerDiv.style.borderColor = 'black';
+innerDiv.style.backgroundColor = 'pink';
+
+// adds h1 within the div element
+const anotherh1 = document.createElement('h1');
+anotherh1.textContent = 'I\'m in a div';
+innerDiv.appendChild(anotherh1);
+
+// adds p to the div element
+const anotherP = document.createElement('p');
+anotherP.textContent = 'ME TOO';
+innerDiv.appendChild(anotherP);
+
+content.appendChild(innerDiv);
+
+// select button and make it do an alert
+const button1 = document.querySelector('#btn1');
+
+function alertFunction() {
+  alert("yayo");
+}
+
+// button1.addEventListener('click', alertFunction);
+button1.addEventListener('click', function(e){
+  e.target.style.backgroundColor = 'blue';
+});
+
+
+
+
+
 
 

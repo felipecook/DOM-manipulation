728b871871788584dab31f003934eb1c971a904f
felipe cook <felipepcook@gmail.com>
2022-03-26 14:16:53 -0400

    manipulating the DOM in chrome with js script

---
+++ app.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is glorious text-content';
+
+container.appendChild(content);
+
